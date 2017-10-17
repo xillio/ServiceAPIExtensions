123e34240825a81185d5175cea3e29a0289da47f
Sander Visser <sander.visser@Xillio.nl>
2017-10-17 15:09:33 +0200

    Changed delete + now only moves objects to wastebasket. Never fully deletes.

--- ServiceAPIExtensions/Controllers/ContentAPiController.cs
+++ ServiceAPIExtensions/Controllers/ContentAPiController.cs
@@ -284,7 +284,6 @@ namespace ServiceAPIExtensions.Controllers
             if (r == ContentReference.RootPage && string.IsNullOrEmpty(Path)) return BadRequest("'root' can only be deleted by specifying its name in the path!");
 
             // If its already in the wastebasket delete it, otherwise put it in the wastebasket.
-            if (_repo.GetAncestors(r).Any(ic => ic.ContentLink == ContentReference.WasteBasket)) _repo.Delete(r, false);
             else _repo.MoveToWastebasket(r);
             return Ok();
         }
